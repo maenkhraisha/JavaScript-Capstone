b03f6e78842dffe9dd3ca55577bc84199f3964ea
maen <maenkh@yahoo.com>
2022-04-19 15:51:27 +0300

    add logo

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import _ from 'lodash';
 import './index.css';
-import icon from './logo.png'
+import "./index.html";
+
+
+// import icon from './logo.png'
 
 
 // const iconel = document.createElement('img');
