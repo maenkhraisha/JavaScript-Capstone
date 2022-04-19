a513259bed186518c96a0e0719c8b4eb9b30a0d4
maen <maenkh@yahoo.com>
2022-04-19 15:58:45 +0300

    styling the header

--- src/index.js
+++ src/index.js
@@ -2,11 +2,3 @@ import _ from 'lodash';
 import './index.css';
 import "./index.html";
 
-
-// import icon from './logo.png'
-
-
-// const iconel = document.createElement('img');
-// iconel.src = icon;
-
-// document.getElementById('logo').appendChild(iconel);
