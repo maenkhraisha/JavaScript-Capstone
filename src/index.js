fbd7698165cb47d034b4694790da73e9fd39ddc3
Shubh <shubh.scb@gmail.com>
2022-04-21 21:30:31 +0530

    display data

--- src/index.js
+++ src/index.js
@@ -4,13 +4,14 @@ import './index.html';
 import 'boxicons';
 import API from './module/API.js';
 import DOM from './module/DOM.js';
-import { postData } from './module/involveApi.js';
-import { getItemFromApi} from './module/involveApi.js';
+// import { postData } from './module/involveApi.js';
+// import { getItemFromApi} from './module/involveApi.js';
 
+const apiUrl = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/kUchJcdqjG77aDKZUYlr/comments/';
+const apiUrl2 = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/kUchJcdqjG77aDKZUYlr/comments?item_id=item1'
 
 let dynamicSection;
 let cbList;
-let innerButtonClick;
 window.addEventListener('load', () => {
   API.getItemsList()
     .then((resolve) => {
@@ -18,12 +19,12 @@ window.addEventListener('load', () => {
     }).then(() => {
       cbList = document.querySelectorAll('.btn-comment');
       cbList.forEach.call(cbList, (cb) => {
-        cb.addEventListener('click',() => {
+        cb.addEventListener('click', () => {
           const popUpWindow = document.createElement('div');
           popUpWindow.className = 'pop-up-window';
 
           const mainImagePopup = document.createElement('img');
-          mainImagePopup.src = 'item.strMealThumb';
+          mainImagePopup.src = 'https://cdn.pixabay.com/photo/2010/12/13/10/05/berries-2277__480.jpg';
           mainImagePopup.className = 'popup-img';
 
           const h2 = document.createElement('h2');
@@ -35,7 +36,7 @@ window.addEventListener('load', () => {
 
           const nameInput = document.createElement('input');
           nameInput.type = 'text';
-          nameInput.classname  = 'name-input-holder';
+          nameInput.classname = 'name-input-holder';
 
           const messageInput = document.createElement('textarea');
           messageInput.className = 'text-area';
@@ -46,27 +47,58 @@ window.addEventListener('load', () => {
 
           const displyText = document.createElement('div');
           displyText.className = 'display-text';
-          
+
           popUpWindow.appendChild(mainImagePopup);
           popUpWindow.appendChild(h2);
+          popUpWindow.appendChild(displyText);
           popUpWindow.appendChild(commentTitle);
           popUpWindow.appendChild(nameInput);
           popUpWindow.appendChild(messageInput);
           popUpWindow.appendChild(innerButton);
           dynamicSection = document.querySelector('.test-dynami')
           dynamicSection.appendChild(popUpWindow);
+
+          innerButton.addEventListener('click', () => {
+            // commentButtonId = cbList.id;
+            fetch(apiUrl, {
+              method: 'POST',
+              body: JSON.stringify({
+                item_id: 'abc123',
+                username: nameInput.value,
+                comment: messageInput.value
+              }),
+              headers: {
+                'Content-type': 'application/json; charset=UTF-8',
+              },
+            }).then((res) => console.log(res.status));
+
+            const commentDataApi = fetch(apiUrl2).then(response => response.json())
+            .then(json => displyText.append(json[0]));
             
+           
+            
+            // console.log(commentDataApi);
+            // commentDataApi.prototype.forEach.call((commentDataApi.item, (item) => {
+            //  console.log(item)
+          //  }));
+
+          });
+
         });
-        });
-    })
+      });
+    });
 });
 
-// .then(() => {
-//   innerButtonClick = document.querySelectorAll('.inner-button');
-//   innerButtonClick.addEventListener('click', () => {
-    
-//   })
-// });
+// const getAllComments = async (id) => {
+//   const allComments = await getComments(id);
+//   commentCount.textContent = `Comments (${allComments.length})`;
+//   commentList.innerHTML = '';
+//   allComments.forEach((e) => {
+//     commentList.innerHTML += `
+//     <li>${e.creation_date}  ${e.username}:  ${e.comment}</li>
+//     `;
+//   });
+// };
 
 
 
@@ -75,4 +107,6 @@ window.addEventListener('load', () => {
 
 
 
-  
+
+
+
--- src/module/involveApi.js
+++ src/module/involveApi.js
@@ -1,6 +1,8 @@
 const apiUrl = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/kUchJcdqjG77aDKZUYlr/comments/';
 const apiUrl2 = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/kUchJcdqjG77aDKZUYlr/comments?item_id=item1'
 
+
+
 export const postData = async () => {
     const awaitData = await fetch(apiUrl, {
         method: 'POST',
@@ -19,12 +21,12 @@ export const postData = async () => {
 console.log('response:', postData());
 
 export const getItemFromApi = fetch(apiUrl2).then(response => response.json())
-.then(json => console.log(json));
+    .then(json => console.log(json));
 
 
 
 
-  
+
 
 
 
