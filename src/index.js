85f8ea249a6d84b3174a0a24bb40cfc97427fe05
maen <maenkh@yahoo.com>
2022-04-22 21:42:17 +0300

    finish display the number of like and add a new like

--- src/index.js
+++ src/index.js
@@ -4,24 +4,12 @@ import './index.html';
 import 'boxicons';
 import API from './module/API.js';
 import DOM from './module/DOM.js';
-import Involvment from './module/involvment_API.js';
 
-const addEventToIcon = () => {
-  const iconList = document.querySelectorAll('.items-like-count');
-  iconList.forEach.call(iconList, (icon) => {
-    icon.addEventListener('click', () => {
-      Involvment.addLike(icon.id);
-    });
-  });
-};
-const load = () => {
-  API.getItemsList().then((resolve) => {
-    DOM.createItems(resolve.meals).then(() => {
-      addEventToIcon();
-    });
-  });
+const build = async () => {
+  const resolve = await API.getItemsList();
+  DOM.createItems(resolve.meals);
 };
 
 window.addEventListener('load', () => {
-  load();
+  build();
 });
